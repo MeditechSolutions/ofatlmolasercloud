6dbc46d8bf81803ceb521d394a689b884325566a
kquiroz <39870381+kquiroz@users.noreply.github.com>
2020-07-29 18:45:05 -0500

    Update res_partner.py

--- mouse_toponyms/models/res_partner.py
+++ mouse_toponyms/models/res_partner.py
@@ -69,7 +69,8 @@ class Partner(models.Model):
         '''
         # get the information that will be injected into the display format
         # get the address format
-        address_format = self.country_id.id == 173 and "%(street)s\n%(district_name)s - %(city_name)s - %(state_name)s - %(zip)s\n%(country_name)s" or self._get_address_format()
+        address_format = self.country_id.id != 173 and self._get_address_format() or '''%(street)s
+%(district_name)s - %(city_name)s - %(state_name)s - %(zip)s - %(country_name)s'''
         args = {
             'district_code': self.l10n_pe_district.code or '',
             'district_name': self.l10n_pe_district.name or '',
